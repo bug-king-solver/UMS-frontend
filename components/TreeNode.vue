ab176deb3f7975d25ff1c0d496e79d7d0c83efe1
leo <janny.dream.high@gmail.com>
2023-07-31 08:20:42 -0700

    feat: updating tree

--- components/TreeNode.vue
+++ components/TreeNode.vue
@@ -6,9 +6,61 @@
       <i v-else class="fa fa-file-alt"></i>
       {{ node.name }}
     </span>
-    <i v-if="hasChildren && !isExpanded" class="fas fa-plus" @click="onPlusIconClick"></i>
+    <a data-bs-toggle="modal" data-bs-target="#modal-block-normal">
+      <i v-if="hasChildren && !isExpanded" class="fas fa-plus"></i>
+    </a>
     <div v-if="hasChildren && isExpanded" class="children-container">
       <TreeNode v-for="child in node.children" :key="child.id" :node="child" @click="handleAction(child)" />
+    </div>
+    <!-- Modal for adding new sub-categories or accounts -->
+    <div
+      class="modal"
+      id="modal-block-normal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="modal-block-normal"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <BaseBlock title="Add new category or account" transparent class="mb-0">
+            <template #options>
+              <button
+                type="button"
+                class="btn-block-option"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <i class="fa fa-fw fa-times"></i>
+              </button>
+            </template>
+
+            <template #content>
+              <div class="block-content fs-sm">
+                <p>
+                  Dropdowns
+                </p>
+              </div>
+              <div class="block-content block-content-full text-end bg-body">
+                <button
+                  type="button"
+                  class="btn btn-sm btn-alt-secondary me-1"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-sm btn-primary"
+                  data-bs-dismiss="modal"
+                >
+                  Perfect
+                </button>
+              </div>
+            </template>
+          </BaseBlock>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +72,11 @@ const { node } = defineProps(['node']);
 
 const isExpanded = ref(false);
 const hasChildren = ref(!!node.children);
+const showingModal = ref(false);
+const selectedType = ref('category');
+const nameEnglish = ref('');
+const nameArabic = ref('');
+const nameKurdish = ref('');
 
 const toggleNode = () => {
   isExpanded.value = !isExpanded.value;
@@ -32,10 +89,37 @@ const handleAction = (child) => {
 };
 
 const onPlusIconClick = () => {
-  // Custom action for the Plus icon
-  // You can perform any specific action here
-  console.log("Plus icon clicked for category:", node.name);
-  // For example, you can show a specific modal or perform an action on the node
+  showingModal.value = true;
+};
+
+const hideModal = () => {
+  showingModal.value = false;
+  resetForm();
+};
+
+const resetForm = () => {
+  selectedType.value = 'category';
+  nameEnglish.value = '';
+  nameArabic.value = '';
+  nameKurdish.value = '';
+};
+
+const addItem = () => {
+  const newItem = {
+    type: selectedType.value,
+    name: {
+      english: nameEnglish.value,
+      arabic: nameArabic.value,
+      kurdish: nameKurdish.value,
+    },
+    children: selectedType.value === 'category' ? [] : undefined,
+    id: Math.random().toString(36).substr(2, 9), // Generate a unique ID
+  };
+
+  node.children = node.children || [];
+  node.children.push(newItem);
+
+  hideModal();
 };
 </script>
 
@@ -73,4 +157,5 @@ const onPlusIconClick = () => {
   margin-left: 5px;
   cursor: pointer;
 }
+
 </style>
